dc13820a1a0e8d6f6df958ba68069463cfe7ec29
james-built <jgbuilt@gmail.com>
2018-11-19 15:49:43 +1300

    nesting components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './first/test.component'
+import { TestComponent } from './first/test.component';
+import { FooComponent } from './foo/foo.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    FooComponent
   ],
   imports: [
     BrowserModule,
