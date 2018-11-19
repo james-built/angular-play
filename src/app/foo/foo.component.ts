f5f071f4f5e625f7f454f03f29fd9bfc3b6240bb
james-built <jgbuilt@gmail.com>
2018-11-20 12:07:56 +1300

    two way data binding example

--- src/app/foo/foo.component.ts
+++ src/app/foo/foo.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class FooComponent implements OnInit {
   satisfyingClick = true
   buttonClickMessage = 'go on, click the button'
-  userMessage = ''
+  userMessage = 'Type a message here'
 
   constructor() {
     
