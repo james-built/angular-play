30b4983fb96dd8ef761b4a16d504d98ae3790b37
james-built <jgbuilt@gmail.com>
2018-11-19 16:04:26 +1300

    styling examples both stylesheet & inline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] // can also do styles inline by using an array of strings containing the styles as follows
+  /* styles: [`
+  p {
+    color: blue;
+  }
+  `]*/
 })
 export class AppComponent {
   name = 'James';
