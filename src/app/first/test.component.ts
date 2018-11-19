2f8ca33ced4fde78cd911438d39634bc4878016c
james-built <jgbuilt@gmail.com>
2018-11-19 15:44:05 +1300

    created test component

---
+++ src/app/first/test.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html'
+}) // decorator function configures this class for angular
+export class TestComponent {
+  
+}
